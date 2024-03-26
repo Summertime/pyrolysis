4bea48e074bb15b587b2f77c1655a985e39d970f
Summer <30243134+Summertime@users.noreply.github.com>
2024-03-26 18:44:25 +1030

    adding reduce

---
+++ functools/mod.ts
@@ -0,0 +1,19 @@
+const unset = Symbol("unset"); // Python checks args length, emulating
+export async function reduce<T>(
+  func: (a: T, b: T) => T,
+  iterable: AsyncIterable<T>,
+  initializer: T = unset,
+): T {
+  let value = initializer;
+  for await (const item of iterable) {
+    if (value === unset) {
+      value = item;
+    } else {
+      value = await func(value, item);
+    }
+  }
+  if (value === unset) {
+    throw new TypeError("reduce() of empty iterable with no initial value");
+  }
+  return value;
+}
